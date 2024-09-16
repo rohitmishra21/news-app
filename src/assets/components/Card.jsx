77f1202034c97495e54cfaad9ad6dc58105d6937
Rohit mishra <mishrarohit27112005@gmail.com>
2024-09-16 11:35:10 +0530

    complete

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,30 +1,34 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-const Card = ({data}) => {
+const Card = ({ data }) => {
 
-
-  
-  console.log(data);
-  
   return (
-    <div className='flex w-full min-h-[300vh] px-24 justify-around flex-wrap gap-2 '>
-   {data.map((curItem,index)=>{
-    return(
-      <>
-      <div key={index} className=' w-[32%]  '>
-      <div className='flex flex-col gap-5 '>
-        <img className='w-full h-full object-cover object-center' src={curItem.urlToImage} alt="" />
-        <h1 className='text-xl'>{curItem.title}</h1>
-        <h1 className='text-xl'>{curItem.description}</h1>
-        <button className='px-7 py-2 bg-blue-950 text-white' onClick={() => window.open(curItem.url)}>read more</button>
-      </div>
-      </div>
-     
-      </>
-    )
-   })}
+    <div className="flex w-full min-h-[300vh] xl:px-24 xl:flex-row flex-col items-center justify-end  xl:justify-around flex-wrap gap-2 ">
+      {data.map((curItem, index) => {
+        return (
+          <>
+            <div key={index} className=" xl:w-[32%]  ">
+              <div className="flex justify flex-col gap-5 ">
+                <img
+                  className="w-full h-full object-cover object-center"
+                  src={curItem.urlToImage}
+                  alt=""
+                />
+                <h1 className="text-xl">{curItem.title}</h1>
+                <h1 className="text-xl">{curItem.description}</h1>
+                <button
+                  className="px-7 mb-6 py-2 bg-blue-950 text-white"
+                  onClick={() => window.open(curItem.url)}
+                >
+                  read more
+                </button>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 
 const Main = () => {
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState("rohit sharma");
   const [newsData, setnewsData] = useState(null);
   const API_KEY = "d2258cba1769419981216d96e4a9273a";
+
+  useEffect(() => {
+    fechData();
+  }, [search]);
 
   const fechData = async () => {
     const responce = await fetch(
@@ -12,61 +16,91 @@ const Main = () => {
     );
     const data = await responce.json();
     setnewsData(data.articles);
-    
   };
 
   function changer(e) {
     setSearch(e.target.value);
   }
 
+  function userInput(event) {
+    setSearch(event.target.value);
+  }
+  function main(eve) {
+    setSearch(eve.target.value);
+  }
+
   return (
     <div className="min-h-screen w-full">
-      <nav className="w-full h-32 flex items-center px-10 justify-between bg-blue-300">
-        <h1 className="text-4xl font-bold">Trendy news</h1>
-        <div className="flex gap-4 text-xl">
-          <h1>All</h1>
-          <h1>News</h1>
-          <h1>Trending</h1>
+      <nav className="xl:w-full h-32 flex xl:items-center xl:flex-row flex-col  xl:px-10 items-end gap-2  xl:justify-between bg-blue-300">
+        <h1 className="xl:text-4xl text-2xl font-bold">Trendy news</h1>
+        <div className="flex xl:gap-4 gap-2 text-xl  font-extrabold">
+          <h1 className="cursor-pointer" onClick={main} value="All">
+            All
+          </h1>
+          <h1 className="cursor-pointer" onClick={main} value="News">
+            News
+          </h1>
+          <h1 className="cursor-pointer" onClick={main} value="Trending">
+            Trending
+          </h1>
         </div>
         <div className="flex">
           <input
             type="text"
-            placeholder="Search"
-            className="w-80"
+            placeholder="Search news"
+            className="xl:w-80 cursor-pointer"
             onChange={changer}
           />
           <button
-            className="px-4 py-2 bg-blue-950 text-white uppercase tracking-wider font-semibold"
+            className="xl:px-4 xl:py-2 bg-blue-950 text-white uppercase cursor-pointer tracking-wider font-semibold"
             onClick={fechData}
           >
-            search
+            search 
           </button>
         </div>
       </nav>
       <div>
-        <h1 className="text-5xl font-bold text-center capitalize py-6">
+        <h1 className="xl:text-5xl font-bold text-center capitalize xl:py-6 py-4 drop-shadow-lg">
           stay update with trendy news
         </h1>
-        <div className="buttons flex text-2xl gap-2 py-6 items-center cursor-pointer justify-center">
-          <h1 className="px-4 py-2 bg-blue-950 text-white rounded-full" >
+        <div className="buttons flex text-xs xl:text-xl gap-2 xl:py-6 py-4 items-center  justify-center">
+          <button
+            className="xl:px-4 xl:py-2 cursor-pointer hover:scale-95 transition-all bg-blue-950 text-white rounded-full px-2 py-1 "
+            onClick={userInput}
+            value="sports"
+          >
             Sports
-          </h1>
-          <h1 className="px-4 py-2 bg-blue-950 text-white rounded-full">
+          </button>
+          <button
+            className="xl:px-4 xl:py-2 cursor-pointer hover:scale-95 transition-all bg-blue-950 text-white rounded-full px-2 py-1 "
+            onClick={userInput}
+            value="Politics"
+          >
             Politics
-          </h1>
-          <h1 className="px-4 py-2 bg-blue-950 text-white rounded-full">
+          </button>
+          <button
+            className="xl:px-4 xl:py-2 cursor-pointer hover:scale-95 transition-all bg-blue-950 text-white rounded-full px-2 py-1 "
+            onClick={userInput}
+            value="Entertenment"
+          >
             Entertenment
-          </h1>
-          <h1 className="px-4 py-2 bg-blue-950 text-white rounded-full">
+          </button>
+          <button
+            className="xl:px-4 xl:py-2 cursor-pointer hover:scale-95 transition-all bg-blue-950 text-white rounded-full px-2 py-1 "
+            onClick={userInput}
+            value="Education"
+          >
             Education
-          </h1>
-          <h1 className="px-4 py-2 bg-blue-950 text-white rounded-full">
+          </button>
+          <button
+            className="xl:px-4 xl:py-2 cursor-pointer hover:scale-95 transition-all bg-blue-950 text-white rounded-full px-2 py-1 "
+            onClick={userInput}
+            value="Health"
+          >
             Health
-          </h1>
+          </button>
         </div>
-        <div>
-          {newsData === null ? null : <Card data={newsData} />}
-        </div>
+        <div>{newsData === null ? null : <Card data={newsData} />}</div>
       </div>
     </div>
   );
